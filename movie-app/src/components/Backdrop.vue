3990a50b069e7b2fe0869b0d37f1205b6db52e2a
Ways Alqorni <wayslos92@gmail.com>
2021-12-21 09:10:29 +0700

    add env

---
+++ movie-app/src/components/Backdrop.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mb-5 mx-auto d-flex justify-content-center">
+    <img
+      class="img-fluid"
+      style="width: 800px;"
+      :src="`${posterPath}${movie.backdrop_path}`"
+      alt="image"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Backdrop',
+  mounted() {
+    const ID = this.$route.params.id
+    this.$store.dispatch('getMovie', ID)
+  },
+  computed: {
+    posterPath() {
+      return this.$store.state.posterPath
+    },
+
+    movie() {
+      return this.$store.state.movie
+    },
+  },
+}
+</script>
+
+<style>
+.detail {
+  margin-bottom: 100px;
+}
+</style>
